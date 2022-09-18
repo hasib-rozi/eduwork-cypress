17df9ef9520ac895b675b8430bb570563e8b8f42
hasibrozi <hasib.rozi@gmail.com>
2022-09-18 16:24:37 +0700

    third test

--- cypress/e2e/0-eduwork-learn/first-test.cy.js
+++ cypress/e2e/0-eduwork-learn/first-test.cy.js
@@ -3,7 +3,8 @@
 describe('My First Test', () => {
     it('Visits the Kitchen Sink', () => {
         cy.visit('https://example.cypress.io')
-
-        cy.contains('type')
+        cy.get('h1').contains('Kitchen Sink')
+        cy.contains('get').click()
+        cy.url().should('include', '/commands/querying')
     })
 })
